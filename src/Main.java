805f9d27318ff1457d4287aeed87b49e4d21003f
USer <user@sdaProtik.com>
2025-07-24 21:33:25 +0200

    c

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,89 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        //Step 1: Declare variables
+        double balance = 0;
+        boolean isRunning = true;
+        int choice;
+
+        //Step 2: Display a menu
+        while (isRunning) {
+            System.out.println("***************");
+            System.out.println("BANKING PROGRAM");
+            System.out.println("***************");
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("***************");
+
+            //Step 3: Get and process users choice
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> showBalance(balance);
+                case 2 -> balance += deposit();
+                case 3 -> balance -= withdraw(balance);
+                case 4 -> isRunning = false;
+                default -> System.out.println("Invalid choice.");
+            }
+        }
+        //Step 7: Create a EXIT message at the end.
+        System.out.println("***************************");
+        System.out.println("Thank you! Have a nice day!");
+        System.out.println("***************************");
+
+
+
+        scanner.close();
+    }
+    //Step 4: Create  a method of showBalance()
+    static void showBalance(double balance){
+        System.out.println("***************");
+        System.out.printf("$%.2f\n", balance);
+    }
+    //Step 5: Another method to add a deposit()
+    static double deposit(){
+
+        double amount;
+
+        System.out.print("Enter an amount to be deposited: ");
+        amount = scanner.nextDouble();
+
+        if (amount < 0){
+            System.out.println("Amount can't be negative");
+            return 0;
+        }
+        else {
+            return amount;
+        }
+
+    }
+    //Step 6: Adding another method to withdraw()
+    static double withdraw(double balance){
+
+        double amount;
+
+        System.out.print("Enter amount to be withdrawn: ");
+        amount = scanner.nextDouble();
+
+        if (amount > balance){
+            System.out.println("***************************");
+            System.out.println("INSUFFICENT FUNDS");
+            return 0;
+
+        }
+        else if (amount < 0) {
+            System.out.println("***************************");
+            System.out.println("Amount can't be negative");
+            return 0;
+
+        }
+        else {
+            return amount;
         }
     }
 }
